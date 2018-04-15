268999d9c110e1293e0736698dbcff5b2b74efcc
fajfara <a.fajfar95@gmail.com>
2018-04-15 18:56:03 +0200

    Updating files and website

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ DAY 13.04.2018
 DAY 14.04.2018
 
 > Just converted everything to scsss, i like it a lot more then just plain css. Added a new color pallet, still not decided on that yet. Also minor JS bug fixes.
+
+DAY 15.04.2018
+
+> Added another item to the portfolio section styling isn't done yet. Added the contacts section and styled it a bit, also added the resources section where i will list all the sources. Currently the website is not responisive so that would be the work for the future. 
--- js/chart-draw.js
+++ js/chart-draw.js
@@ -7,27 +7,31 @@ $(document).ready(function(){
     var ctx = $("#myChart");
     
     function drawGraph(data){
+        console.log(data);
         HTML = data.languages.HTML.xps;
         xpsCSS = data.languages.CSS.xps;
         JS = data.languages.JavaScript.xps;
+        SCSS = data.languages.SCSS.xps
         Python = data.languages.Python.xps;
         Chart.defaults.global.defaultFontColor = '#fff';
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['HTML', 'CSS', 'Javascript', 'Python'],
+                labels: ['HTML', 'CSS','SCSS', 'Javascript', 'Python'],
                 datasets: [{
                     label: 'Total XP by programming language',
-                    data: [HTML, xpsCSS, JS, Python],
+                    data: [HTML, xpsCSS, SCSS, JS, Python],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)'
+                        'rgba(255, 99, 132, 0.5)',
+                        'rgba(54, 162, 235, 0.5)',
+                        'rgba(55, 235, 229, 1)',
+                        'rgba(255, 206, 86, 0.5)',
+                        'rgba(75, 192, 192, 0.5)'
                     ],
                     borderColor: [
                         'rgba(255,99,132,1)',
                         'rgba(54, 162, 235, 1)',
+                        'rgba(55, 235, 229, 1)',
                         'rgba(255, 206, 86, 1)',
                         'rgba(75, 192, 192, 1)'
                     ],
